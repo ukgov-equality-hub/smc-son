1c385ffac21e0c13f7ffcc2e4dee990c55bb07df
Suganya Sivaskantharajah <suganya.sivaskantharajah@cabinetoffice.gov.uk>
2025-08-29 13:05:11 +0100

    EHD-1875 - DR55

---
+++ R/DR55.R
@@ -0,0 +1,120 @@
+
+#################################################
+# INPUTS FOR THIS SCRIPT - CHANGE THIS SECTION
+
+input_folder = "input/SON25/"
+
+input_file = "2025-01-01-dr55-full-dataset.csv"
+
+output_folder_prefix = "../son/content"
+domain = "drivers_of_social_mobility"
+subdomain = "research_and_development_environment"
+indicator_name = "gva_per_capita"
+version = "1.0"
+
+indicator_code = "DR55"
+
+
+
+############
+# IMPORTS
+
+source("_shared.R")
+
+###############
+# THE SCRIPT
+
+## Clear output folder
+unlink(output_folder, recursive=TRUE)
+
+input_file_path = paste0(input_folder, input_file)
+
+# Open the input file
+data = read.csv(input_file_path)
+
+# Remove some columns
+data = data_frame__remove_columns(
+  data,
+  "variable_used",
+  "age_used",
+  "weight_used",
+  "year_used",
+  "label"
+)
+
+csv_filename = generate_csv_file_name()
+save_data_frame(data, csv_filename)
+
+
+
+##########################################
+# SECTION: By year
+
+
+section_chart_type = "trend"
+section_csv_name = "year-and-country"
+
+data_for_section = get_data_for_chart_type(data, section_chart_type)
+
+#################
+# CHART FORMAT
+
+data_for_section = data_frame__sort_rows_with_specific_values(
+  data_frame = data_for_section,
+  column_1 = "primary_split_value",
+  values_1 = nations_order,
+  column_2 = "secondary_split_value",
+  values_2 = NULL
+)
+
+csv_filename = generate_csv_file_name(split = section_csv_name, format = "chart")
+save_data_frame(data_for_section, csv_filename)
+
+#################
+# TABLE FORMAT
+
+pivot_table = pivot_table__create(
+  pivot_table_source = data_for_section,
+  pivot_columns_column_name = "primary_split_value",
+  pivot_rows_column_name = "secondary_split_value",
+  pivot_cells_column_name = "value",
+  pivot_table_name = "Year",
+  pivot_table_rows_order_values = sort(
+    unique(data_for_section$secondary_split_value), decreasing=TRUE),
+  pivot_table_columns_order_values = nations_order,
+  pivot_table_column_names_suffix = " (£)"
+  ) 
+
+csv_filename = generate_csv_file_name(split = section_csv_name, format = "table")
+save_data_frame(pivot_table, csv_filename)
+
+
+
+##########################################
+# SECTION: By area
+
+section_chart_type = "geography"
+section_csv_name = "ITL2-region-2025"
+
+data_for_section = get_data_for_chart_type(data, section_chart_type)
+
+#################
+# CHART FORMAT
+
+csv_filename = generate_csv_file_name(split = section_csv_name, format = "chart")
+save_data_frame(data_for_section, csv_filename)
+
+#################
+# TABLE FORMAT
+
+pivot_table = pivot_table__create(
+  pivot_table_source = data_for_section,
+  pivot_columns_column_name = "unit",
+  pivot_rows_column_name = "primary_split_value",
+  pivot_cells_column_name = "value",
+  pivot_table_name = "Region"
+) %>% rename("GVA per Capita (£)" = "£")
+
+
+csv_filename = generate_csv_file_name(split = section_csv_name, format = "table")
+save_data_frame(pivot_table, csv_filename)
