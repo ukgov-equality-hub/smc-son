55da9f26e37e121dd1f431cc77f62d072150805b
Suganya Sivaskantharajah <suganya.sivaskantharajah@cabinetoffice.gov.uk>
2025-10-21 17:22:57 +0100

    EHD-1956 bring IN31 SEB section back

--- R/IN31.R
+++ R/IN31.R
@@ -46,6 +46,45 @@ csv_filename = generate_csv_file_name()
 save_data_frame(data, csv_filename)
 
 
+###########################
+# SECTION: By SEB
+
+section_chart_type = "seb"
+section_csv_name = "SEB"
+
+
+data_for_section = get_data_for_chart_type(data, section_chart_type)
+
+
+#################
+# CHART FORMAT
+
+data_for_section = data_frame__sort_rows_with_specific_values(
+  data_frame = data_for_section,
+  column_1 = "secondary_split_value",
+  values_1 = occupational_class_order
+)  %>% filter(!is.na(secondary_split_value))
+
+csv_filename = generate_csv_file_name(split = section_csv_name, format = "chart")
+save_data_frame(data_for_section, csv_filename)
+
+#################
+# TABLE FORMAT
+
+pivot_table = pivot_table__create(
+  pivot_table_source = data_for_section,
+  pivot_columns_column_name = "unit",
+  pivot_rows_column_name = "secondary_split_value",
+  pivot_cells_column_name = "value",
+  pivot_table_name = "Socio-economic background",
+  pivot_table_rows_order_values = rev(occupational_class_order),
+  pivot_table_column_names_suffix = " (%)"
+) %>% rename("Percentage (%)" = "Percent (%)")
+
+csv_filename = generate_csv_file_name(split = section_csv_name, format = "table")
+save_data_frame(pivot_table, csv_filename)
+
+
 
 ##########################################
 # SECTION: Moving average
@@ -267,3 +306,4 @@ pivot_table = pivot_table__create(
 
 csv_filename = generate_csv_file_name(split = section_csv_name, format = "table")
 save_data_frame(pivot_table, csv_filename)
+
