7ea038e225122843a23ef329e7120c28e374ad09
James Griffiths <james@jwgsoftware.com>
2026-05-11 14:49:54 +0100

    EHD-2081: Move SON to subfolder: Redirect old SON URLs to new /state-of-the-nation routes

--- son/redirects/__init__.py
+++ son/redirects/__init__.py
@@ -8,21 +8,74 @@ logger = Logger()
 
 @redirects.route('/drivers_of_social_mobility/research_and_development_environment/university_research_students', methods=['GET'])
 def DR53_university_research_students():
-    return redirect("/drivers_of_social_mobility/research_and_development_environment/postgraduate_education")
+    return redirect("/state-of-the-nation/drivers_of_social_mobility/research_and_development_environment/postgraduate_education/latest")
 
 
 @redirects.route('/social_mobility_by_area', methods=['GET'])
 def area_home_page_without_version():
-    return redirect("/social_mobility_by_area/latest")
+    return redirect("/state-of-the-nation/social_mobility_by_area/latest")
+
+
+@redirects.route('/state-of-the-nation/social_mobility_by_area', methods=['GET'])
+def son_area_home_page_without_version():
+    return redirect("/state-of-the-nation/social_mobility_by_area/latest")
+
+
+@redirects.route('/social_mobility_by_area/latest', methods=['GET'])
+def area_home_page_latest():
+    return redirect("/state-of-the-nation/social_mobility_by_area/latest")
+
+
+@redirects.route('/social_mobility_by_area/<major_version>.<minor_version>', methods=['GET'])
+def area_home_page_with_version(major_version, minor_version):
+    return redirect(f"/state-of-the-nation/social_mobility_by_area/{major_version}.{minor_version}")
 
 
 @redirects.route('/social_mobility_by_area/<area>', methods=['GET'])
 def area_page(area):
-    return redirect(f"/social_mobility_by_area/ITL2_regions/{area}")
+    return redirect(f"/state-of-the-nation/social_mobility_by_area/ITL2_regions/{area}")
+
+
+@redirects.route('/social_mobility_by_area/ITL2_regions/<area>', methods=['GET'])
+def area_page_ITL2(area):
+    return redirect(f"/state-of-the-nation/social_mobility_by_area/ITL2_regions/{area}")
+
+
+@redirects.route('/social_mobility_by_area/203_regions/<area>', methods=['GET'])
+def area_page_203_regions(area):
+    return redirect(f"/state-of-the-nation/social_mobility_by_area/203_regions/{area}")
+
+
+@redirects.route('/social_mobility_by_area/205_regions/<area>', methods=['GET'])
+def area_page_205_regions(area):
+    return redirect(f"/state-of-the-nation/social_mobility_by_area/205_regions/{area}")
+
+
+@redirects.route('/<domain>', methods=['GET'])
+def domain_page(domain):
+    return redirect(f"/state-of-the-nation/{domain}")
 
 
 @redirects.route('/<domain>/<subdomain>/<indicator>', methods=['GET'])
 def indicator_page_without_version(domain, subdomain, indicator):
-    return redirect(f"/{domain}/{subdomain}/{indicator}/latest")
+    return redirect(f"/state-of-the-nation/{domain}/{subdomain}/{indicator}/latest")
 
 
+@redirects.route('/<domain>/<subdomain>/<indicator>/latest', methods=['GET'])
+def indicator_page_latest(domain, subdomain, indicator):
+    return redirect(f"/state-of-the-nation/{domain}/{subdomain}/{indicator}/latest")
+
+
+@redirects.route('/<domain>/<subdomain>/<indicator>/<major_version>.<minor_version>', methods=['GET'])
+def indicator_page_with_version(domain, subdomain, indicator, major_version, minor_version):
+    return redirect(f"/state-of-the-nation/{domain}/{subdomain}/{indicator}/{major_version}.{minor_version}")
+
+
+@redirects.route('/<domain>/<subdomain>/<indicator>/<major_version>.<minor_version>/<csv_file_name>.csv', methods=['GET'])
+def csv_file_download(domain, subdomain, indicator, major_version, minor_version, csv_file_name):
+    return redirect(f"/state-of-the-nation/{domain}/{subdomain}/{indicator}/{major_version}.{minor_version}/{csv_file_name}.csv")
+
+
+@redirects.route('/about-our-analysis', methods=['GET'])
+def about_our_analysis():
+    return redirect(f"/state-of-the-nation/about-our-analysis")
