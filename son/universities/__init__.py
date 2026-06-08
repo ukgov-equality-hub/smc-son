0dcfc6cdcde09ff789abaef6da3e1955d46163f8
James Griffiths <james@jwgsoftware.com>
2026-06-08 16:44:15 +0100

    EHD-2084: By-universities pages: University names and rankings

--- son/universities/__init__.py
+++ son/universities/__init__.py
@@ -1,6 +1,9 @@
 import csv
 import os
-from flask import Blueprint, render_template, request, abort
+from pathlib import Path
+
+from flask import Blueprint, render_template, request, abort, send_file
+
 from son.utils.menu import universities_menu
 
 universities = Blueprint('universities', __name__)
@@ -26,11 +29,26 @@ def by_university_homepage():
 
 @universities.route('/universities/by-university/<university_slug>', methods=['GET'])
 def by_university(university_slug):
+    def get_row_from_csv_file(csv_file_path, column_name, search_value):
+        with open(csv_file_path, encoding='utf-8-sig', errors='ignore') as csv_file:
+            reader = csv.DictReader(csv_file)
+
+            for row in reader:
+                if row[column_name] == search_value:
+                    return row
+        return None
+
+    file_path = f"{os.path.dirname(os.path.realpath(__file__))}/../content/universities/university_rankings.csv"
+    row_for_university = get_row_from_csv_file(file_path, 'university_slug', university_slug)
+    if row_for_university is None:
+        abort(404)
+
     return render_template(
         'universities/by-university.html',
         path=request.path,
         universities_menu=universities_menu,
-        university_slug=university_slug
+        university_slug=university_slug,
+        row_for_university=row_for_university
     )
 
 
