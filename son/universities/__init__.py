9ea7987176720468f505dc55290d10bde0d3c681
James Griffiths <james@jwgsoftware.com>
2026-06-08 17:00:56 +0100

    EHD-2084: By-universities pages: Deciles charts and data table

--- son/universities/__init__.py
+++ son/universities/__init__.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 from flask import Blueprint, render_template, request, abort, send_file
 
+from son.utils.get_markdown_content import get_markdown_content
 from son.utils.menu import universities_menu
 
 universities = Blueprint('universities', __name__)
@@ -38,18 +39,41 @@ def by_university(university_slug):
                     return row
         return None
 
-    file_path = f"{os.path.dirname(os.path.realpath(__file__))}/../content/universities/2026/university_rankings.csv"
-    row_for_university = get_row_from_csv_file(file_path, 'university_slug', university_slug)
+    file_path_rankings = f"{os.path.dirname(os.path.realpath(__file__))}/../content/universities/2026/university_rankings.csv"
+    row_for_university = get_row_from_csv_file(file_path_rankings, 'university_slug', university_slug)
     if row_for_university is None:
         abort(404)
+
+    file_path_content = f"{os.path.dirname(os.path.realpath(__file__))}/../content/universities/2026/university-content.md"
+    if not Path(file_path_content).is_file():
+        abort(404)
+
+    f = open(file_path_content, 'r')
+    markdown_text = f.read()
+    f.close()
+
+    markdown_text = markdown_text.replace('PATH_TO_CSV__WITHOUT_LOCATION_WEIGHTING', f"/universities/by-university/{university_slug}--without-location-weighting--chart-format.csv")
+    markdown_text = markdown_text.replace('PATH_TO_CSV__WITH_LOCATION_WEIGHTING', f"/universities/by-university/{university_slug}--with-location-weighting--chart-format.csv")
+    markdown_text = markdown_text.replace('PATH_TO_CSV__TABLE_FORMAT', f"by-university/{university_slug}--table-format.csv")
+
+    markdown_file_parent_directory = os.path.dirname(file_path_content)
+
+    university_content = get_markdown_content(markdown_text, 'UNIV', markdown_file_parent_directory)
 
     return render_template(
         'universities/by-university.html',
         path=request.path,
         universities_menu=universities_menu,
         university_slug=university_slug,
-        row_for_university=row_for_university
+        row_for_university=row_for_university,
+        university_content=university_content
     )
+
+
+@universities.route('/universities/by-university/<filename>.csv', methods=['GET'])
+def by_university_csv_file(filename: str):
+    file_path = f"{os.path.dirname(os.path.realpath(__file__))}/../content/universities/2026/by-university/{filename}.csv"
+    return send_file(file_path)
 
 
 @universities.route('/universities/rankings', methods=['GET'])
