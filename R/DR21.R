a8d30afe8a3de495f39d3a465cd115506be3ac5b
James Griffiths <james@jwgsoftware.com>
2026-08-12 12:58:53 +0100

    EHD-2324: DR21: R code, CSVs, graphs, charts

--- R/DR21.R
+++ R/DR21.R
@@ -2,15 +2,15 @@
 #################################################
 # INPUTS FOR THIS SCRIPT - CHANGE THIS SECTION
 
-input_folder = "input/SON25/"
+input_folder = "input/SON26/"
 
-input_file = "2025-01-01-dr21-full-dataset.csv"
+input_file = "2026-01-01-dr21-full-dataset.csv"
 
 output_folder_prefix = "../son/content/son"
 domain = "drivers_of_social_mobility"
 subdomain = "educational_opportunities_and_quality_of_schooling"
 indicator_name = "further_education_and_training_opportunities"
-version = "3.0"
+version = "4.0"
 
 indicator_code = "DR21"
 
