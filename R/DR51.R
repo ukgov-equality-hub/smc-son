6a4a6071cfa0f6571fc388a4f4eaac541d4cfaf7
James Griffiths <james@jwgsoftware.com>
2026-08-13 13:32:18 +0100

    EHD-2324: DR51: R code, CSVs, graphs, charts

--- R/DR51.R
+++ R/DR51.R
@@ -2,15 +2,15 @@
 #################################################
 # INPUTS FOR THIS SCRIPT - CHANGE THIS SECTION
 
-input_folder = "input/SON25/"
+input_folder = "input/SON26/"
 
-input_file = "2025-01-01-dr51-full-dataset.csv"
+input_file = "2026-01-01-dr51-full-dataset.csv"
 
 output_folder_prefix = "../son/content/son"
 domain = "drivers_of_social_mobility"
 subdomain = "research_and_development_environment"
 indicator_name = "broadband_speed"
-version = "3.0"
+version = "4.0"
 
 indicator_code = "DR51"
 
