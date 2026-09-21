52afcf38e0f11d7f7c1a38a9b0330b9f7512d304
James Griffiths <james@jwgsoftware.com>
2026-09-21 22:13:25 +0100

    EHD-2324: IN36: R code, CSVs, graphs, charts

--- R/IN36.R
+++ R/IN36.R
@@ -2,15 +2,15 @@
 #################################################
 # INPUTS FOR THIS SCRIPT - CHANGE THIS SECTION
 
-input_folder = "input/SON25/"
+input_folder = "input/SON26/"
 
-input_file = "2025-01-01-in36-full-dataset.csv"
+input_file = "2026-01-01-in36-full-dataset.csv"
 
 output_folder_prefix = "../son/content/son"
 domain = "intermediate_outcomes"
 subdomain = "work_in_early_adulthood_(25_to_29_years)"
 indicator_name = "direct_effect_of_social_origin_on_earnings"
-version = "3.0"
+version = "4.0"
 
 indicator_code = "IN36"
 
@@ -54,6 +54,7 @@ section_csv_name = "SEB"
 
 
 data_for_section = get_data_for_chart_type(data, section_chart_type)
+data_for_section <- data_for_section[data_for_section$primary_split_value != "Total", ]
 
 #################
 # CHART FORMAT
@@ -92,6 +93,7 @@ section_csv_name = "moving-average"
 
 
 data_for_section = get_data_for_chart_type(data, section_chart_type)
+data_for_section <- data_for_section[data_for_section$primary_split_value != "Total", ]
 
 #################
 # CHART FORMAT
@@ -147,9 +149,9 @@ data_for_section = get_data_for_chart_type(data, section_chart_type)
 data_for_section = data_frame__sort_rows_with_specific_values(
   data_frame = data_for_section,
   column_1 = "secondary_split_value",
-  values_1 = men_women_order,
+  values_1 = women_men_order,
   column_2 = "primary_split_value",
-  values_2 = occupational_class_order
+  values_2 = rev_occupational_class_order
 )
 
 csv_filename = generate_csv_file_name(split = section_csv_name, format = "chart")
@@ -165,7 +167,7 @@ pivot_table = pivot_table__create(
   pivot_cells_column_name = "value",
   pivot_table_name = "Socio-economic background",
   pivot_table_rows_order_values = rev_occupational_class_order,
-  pivot_table_columns_order_values = men_women_order,
+  pivot_table_columns_order_values = women_men_order,
   pivot_table_column_names_suffix = " (£)"
 )
 
@@ -188,10 +190,8 @@ data_for_section = get_data_for_chart_type(data, section_chart_type)
 
 data_for_section = data_frame__sort_rows_with_specific_values(
   data_frame = data_for_section,
-  column_1 = "secondary_split_value",
-  values_1 = ethnicity_order,
-  column_2 = "primary_split_value",
-  values_2 = occupational_class_order_two_vals
+  column_1 = "primary_split_value",
+  values_1 = rev(occupational_class_order_two_vals)
 )
 
 csv_filename = generate_csv_file_name(split = section_csv_name, format = "chart")
@@ -224,15 +224,26 @@ section_csv_name = "SEB-and-disability"
 
 data_for_section = get_data_for_chart_type(data, section_chart_type)
 
+data_for_section$secondary_split_value <- replace(
+  data_for_section$secondary_split_value,
+  data_for_section$secondary_split_value == "Yes",
+  "Disabled"
+)
+data_for_section$secondary_split_value <- replace(
+  data_for_section$secondary_split_value,
+  data_for_section$secondary_split_value == "No",
+  "Not disabled"
+)
+
 #################
 # CHART FORMAT
 
 data_for_section = data_frame__sort_rows_with_specific_values(
   data_frame = data_for_section,
   column_1 = "secondary_split_value",
-  values_1 = disability_status_no_yes_order,
+  values_1 = c("Disabled", "Not disabled"),
   column_2 = "primary_split_value",
-  values_2 = occupational_class_order,
+  values_2 = rev_occupational_class_order,
 )
 
 csv_filename = generate_csv_file_name(split = section_csv_name, format = "chart")
@@ -248,8 +259,9 @@ pivot_table = pivot_table__create(
   pivot_cells_column_name = "value",
   pivot_table_name = "Socio-economic background",
   pivot_table_rows_order_values = rev_occupational_class_order,
-  pivot_table_columns_order_values = rev(disability_status_no_yes_order)
-) %>% rename("Disabled (£)" = "Yes", "Not disabled (£)" = "No")
+  pivot_table_columns_order_values = c("Disabled", "Not disabled"),
+  pivot_table_column_names_suffix = " (£)"
+)
 
 csv_filename = generate_csv_file_name(split = section_csv_name, format = "table")
 save_data_frame(pivot_table, csv_filename)
